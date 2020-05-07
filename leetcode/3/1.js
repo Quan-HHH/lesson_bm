1a05cae5405c3dcf039ef44dbb08e40b546db14a
Quan-HHH <1411514716@qq.com>
2020-05-07 18:21:08 +0800

    无重复字符的最长子串

--- leetcode/3/1.js
+++ leetcode/3/1.js
@@ -1,14 +1,17 @@
 // 无重复字符的最长字串
-var lengthOfLongestSubstring = function(s) {
+var lengthOfLongestSubstring = function (s) {
     var maxStr = 0;
-    for(let i = 0; i < s.length - maxStr; i++){
-        let tempStr = []
-        if(tempStr.some(ch => ch == maxStr.charAt(i))){
-            continue
-        }else{
-
-            
+    var subString = [];
+    for (let i = 0; i < s.length; i++) {
+        let index = subString.indexOf(s[i]);
+        if (index != -1) { // 在maxStr中 
+            subString.splice(0, index + 1);
+            // console.log(index, subString)
         }
+        subString.push(s.charAt(i));
+        maxStr = subString.length > maxStr ? subString.length : maxStr;
+
     }
+    return maxStr
 };
 console.log(lengthOfLongestSubstring('nbhibbkcds'))
