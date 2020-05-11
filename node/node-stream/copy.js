fa1192f84416b382add9841886642040a242dcd4
Quan-HHH <1411514716@qq.com>
2020-05-11 17:40:02 +0800

    axios源码分析 url拼接

--- node/node-stream/copy.js
+++ node/node-stream/copy.js
@@ -19,4 +19,12 @@ function copy(sourceUrl, destUrl) {
     // 这个pipe就相当于 linux中的管道操作符 |
 }
 
+// 不用流： 读完一个文件得到内容info 变量放到计算机内存中 把他写到某个地方
+// 区别
+// 前端
+// 我： baidu.com -> html,css,js 浏览器 -> 我看到
+// 浏览器执行js, 浏览器申请内存 浏览器在各自的电脑上玉兴  天然就是分布式的
+
+// 后端
+// baidu.com 1台服务器 处理 很多人的请求
 copy('./readme.md', './copy.md')
