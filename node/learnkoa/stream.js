f89c047fa33b1801a1486c66d44fc3653c4add2c
Quan-HHH <1411514716@qq.com>
2020-05-17 10:55:13 +0800

    koa-static处理静态资源

--- node/learnkoa/stream.js
+++ node/learnkoa/stream.js
@@ -1,14 +1,22 @@
 const fs = require('fs');
 const Koa = require('koa');
 const app = new Koa()
+const koaStatic = require('koa-static')
 
 const main = ctx => {
-    console.log(ctx.request.url)
+    // console.log(ctx.request.url)
     // if(ctx.request.url === '/img.jpg') {
     //     console.log('hhh')
     //     ctx.response.type = 'image';
     //     ctx.response.body = fs.createReadStream('./img.jpg');
-    // }else{ 
+    // }else if(ctx.request.url == '/common.css') {
+    //     ctx.response.type = 'css';
+    //     ctx.response.body = fs.createReadStream('./common.css')
+    // }else if(ctx.request.url == '/common.js') {
+    //     ctx.response.type = 'js';
+    //     ctx.response.body = fs.createReadStream('./common.js')
+    // }
+    // else{ 
         ctx.response.type = 'html';  // 响应头 png js css
         // 在服务器端打开可读流, 读这个文件
         ctx.response.body = fs.createReadStream('./template.html');
@@ -23,6 +31,8 @@ const main = ctx => {
 //         ctx.response.body = 'Hello World';
 //     }
 // }
-
+app.use(koaStatic('./'))
 app.use(main)
+
+
 app.listen(8080)
