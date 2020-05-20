691eaa3705a355accf927939eea6e54ad1bc67b5
Quan-HHH <1411514716@qq.com>
2020-05-20 11:36:28 +0800

    swiper轮播

--- demo/cloud_music/common.js
+++ demo/cloud_music/common.js
@@ -1,5 +1,6 @@
 window.onload = function () {
     const tabLinks = document.querySelectorAll('.tabbar a');
+    const swiper_wrapper = document.querySelector('.swiper-wrapper');
     // console.log(tabLinks)
     tabLinks.forEach(function (link) {
         // console.log(link)
@@ -16,8 +17,19 @@ window.onload = function () {
         .then(function (data) {
             return data.json()
         }).then(function (data) {
-            console.log(data)
+            console.log(data.banners[0].imageUrl)
+            console.log(data.banners.length)
+            for(let i = 0; i < data.banners.length; i++) {
+                let banner = document.createElement('div');
+                banner.classList.add('swiper-slide');
+                let img = document.createElement('img');
+                swiper_wrapper.appendChild(banner);
+                banner.appendChild(img);
+                img.setAttribute('src', data.banners[i].imageUrl)
+            }
             })
     //渲染上页面
+    
+
 
 }
