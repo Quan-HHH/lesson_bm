fb0bf0b136404f4c1f10374d98871c8478a981cd
Quan-HHH <1411514716@qq.com>
2020-05-22 14:30:03 +0800

    node工具篇-puppeteer

---
+++ node/node-puppeteer/pdf.js
@@ -0,0 +1,29 @@
+// async await
+const fs = require('fs');
+const promise1 = new Promise((resolve, reject) => {
+    // 回调
+  fs.readFile('./package.json', (err, info) => {
+    resolve(info);
+  })
+})
+const promise2 = (info) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./p.json', info, (err) => {
+      if (!err) {
+        resolve();
+      }
+    })
+  })
+}
+
+// 有些地方会说是异步转同步
+// 事实上 只是写起来像同步 执行起来还是异步
+// then
+promise1.then((info) => {
+  // 返回 promise
+  return promise2(info);
+})
+.then(() => {
+  // 等着 前面 这个 promise
+  console.log('读写完成');
+})
