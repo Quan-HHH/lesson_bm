32cf809bb06adca64ecf7e3bdd01ba9e2874a222
Quan-HHH <1411514716@qq.com>
2020-05-23 16:19:02 +0800

    跨域

--- node/跨域/node.js
+++ node/跨域/node.js
@@ -1,13 +1,22 @@
 const http = require('http');
 
 http.createServer((req, res) => {
+    let origin = req.origin;
     res.writeHead(200, {
-        'Access-Control-Allow-Origin': 'http://127.0.0.1:8080',
-    //     'Access-Control-Allow-Methods': '*', //* 代表所有 也可以写具体的值
-    //     'Access-Control-Allow-Headers': 'Content-Type',
-    //     'Access-Control-Allow-Credentials': true     // Credential凭证  允许前端请求携带cookie
+        'Access-Control-Expose-Headers': 'Access-Control-Allow-Origin', //前端拿到的响应头数据有限 加入到这里面 前端就可以拿到
+        'Access-Control-Allow-Origin': '*',
+        // 'Access-Control-Allow-Origin': 'origin',
+        'Access-Control-Allow-Methods': '*', //* 代表所有 也可以写具体的值
+        'Access-Control-Allow-Headers': 'content-type, X-Requested-With',
+        'Access-Control-Max-Age': -1, //preflight 缓存的时间
+        // 'Access-Control-Allow-Credentials': true     
+        // Credential凭证  允许前端请求携带cookie
+    //   为true的时候 'Access-Control-Allow-Origin'不能设为*
+
     //     // cookie localStorage
     //     // cookie会随着http请求头 自动发送
+    // cookie 存在前端  但前后端都可以设置它 通常会和服务器会话一起用
+    // 前端设置 documen.cookie = 'abc=123'
     })
     let posts = ['js', 'php'];
     res.end(JSON.stringify(posts));
--- node/跨域/readme.md
+++ node/跨域/readme.md
@@ -1,5 +1,5 @@
 ## 跨域
-浏览器得安全策略
+浏览器的安全策略  只有在浏览器才有这个限制 服务端是没有跨域这个限制的
 
 1. 一个源向另一个源发生请求的时候
 同源: 同协议(http/https) 同域名 同端口
@@ -16,7 +16,14 @@ CORS: cross origin resource share
 请求头: accept
 响应头: Set-Cookie
 
-通用头:
+通用头:前后端都可以设置
  Content-Type
  - 前端请求的时候带给后端内容的类型: (3种)  www-url..., text/plain, formdata
  - 后端告诉前端 我给的资源的类型:MIME
+
+## 简单请求 
+跨域直接发出请求
+
+## 非简单请求
+ - OPTIONS prelight   /api
+ - POST  /api
