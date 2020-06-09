8a237a1fd3eb68c3eb60104b74cf8f497a59f599
Quan-HHH <1411514716@qq.com>
2020-06-09 22:14:10 +0800

    react musicApp better-scroll

--- react/mango-music-chapter/src/api/recommend.js
+++ react/mango-music-chapter/src/api/recommend.js
@@ -1,50 +1,20 @@
-export function getNewAlbum() { // 模块化到api 目录 
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        // 
-        resolve([{
-          id: 1,
-          img: 'http://p1.music.126.net/_PsKVKgTAhOvXJmL-r9KlQ==/109951165041015191.jpg?imageView=1&type=webp&thumbnail=370x0',
-          name: '断桥',
-          singer: '程小溪',
-          publicTime: '2018-01-28'
-        },{
-            id: 2,
-            img: 'http://p1.music.126.net/_PsKVKgTAhOvXJmL-r9KlQ==/109951165041015191.jpg?imageView=1&type=webp&thumbnail=370x0',
-            name: '断桥',
-            singer: '程小溪',
-            publicTime: '2018-01-28'
-          },{
-            id: 3,
-            img: 'http://p1.music.126.net/_PsKVKgTAhOvXJmL-r9KlQ==/109951165041015191.jpg?imageView=1&type=webp&thumbnail=370x0',
-            name: '断桥',
-            singer: '程小溪',
-            publicTime: '2018-01-28'
-          },{
-            id: 4,
-            img: 'http://p1.music.126.net/_PsKVKgTAhOvXJmL-r9KlQ==/109951165041015191.jpg?imageView=1&type=webp&thumbnail=370x0',
-            name: '断桥',
-            singer: '程小溪',
-            publicTime: '2018-01-28'
-          },{
-            id: 5,
-            img: 'http://p1.music.126.net/_PsKVKgTAhOvXJmL-r9KlQ==/109951165041015191.jpg?imageView=1&type=webp&thumbnail=370x0',
-            name: '断桥',
-            singer: '程小溪',
-            publicTime: '2018-01-28'
-          },{
-            id: 6,
-            img: 'http://p1.music.126.net/_PsKVKgTAhOvXJmL-r9KlQ==/109951165041015191.jpg?imageView=1&type=webp&thumbnail=370x0',
-            name: '断桥',
-            singer: '程小溪',
-            publicTime: '2018-01-28'
-          },{
-            id: 7,
-            img: 'http://p1.music.126.net/_PsKVKgTAhOvXJmL-r9KlQ==/109951165041015191.jpg?imageView=1&type=webp&thumbnail=370x0',
-            name: '断桥',
-            singer: '程小溪',
-            publicTime: '2018-01-28'
-          },])
-      }, 3000);
-    })
+// 模块化 排行榜的所有接口 一个文件干一件事情
+import jsonp from './jsonp'; //真正的请求 跨域请求解决方案 来自于腾讯的数据
+import { URL, PARAM, OPTION } from './config'
+export function getNewAlbum() {
+  const data = Object.assign({}, PARAM, {
+    g_tk: 1278911659,
+    hostUin: 0,
+    platform: "yqq",
+    needNewCode: 0,
+    data: `{"albumlib":
+  {"method":"get_album_by_tags","param":
+  {"area":1,"company":-1,"genre":-1,"type":-1,"year":-1,"sort":2,"get_tags":1,"sin":0,"num":50,"click_albumid":0},
+  "module":"music.web_album_library"}}`
+  });
+  const OPTION = {
+    param: "callback",
+    prefix: "callback"
   }
+  return jsonp(URL.newalbum, data, OPTION);
+}
---
+++ react/mango-music-chapter/src/common/scroll/Scroll.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import BScroll from 'better-scroll'
+import './scroll.styl'
+
+
+// Scroll 未来会是一个容器 父组件
+class Scroll extends React.Component {
+    componentDidMount() {
+        this.scrollView = ReactDOM.findDOMNode(this.refs.scrollView);
+        // console.log(this.scrollView)
+        if (!this.bScroll) { //防止多次渲染 单例
+            this.bScroll = new BScroll(this.scrollView, {
+                probeType: 3,
+                click: this.props.click
+            })
+            if (this.props.onScroll) {
+                this.bScroll.on('scroll', (scroll) => {
+                    this.props.onScroll(scroll);
+                })
+            }
+        }
+    }
+    refresh() {
+        if (this.bScroll) {
+            this.bScroll.refresh();
+        }
+    }
+    render() {
+        return (
+            <div className="scroll-view" ref="scrollView">
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+Scroll.defaultProps = {
+    click: true,
+    onScroll: null,
+    refresh: false
+}
+Scroll.PropTypes = {
+    click: PropTypes.bool,
+    onScroll: PropTypes.func,
+    refresh: PropTypes.bool
+}
+
+export default Scroll;
