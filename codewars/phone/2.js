15d48ff91af058406d13be9fc6001423e096d826
Quan-HHH <1411514716@qq.com>
2020-07-01 16:14:47 +0800

    codewars

---
+++ codewars/phone/2.js
@@ -0,0 +1,7 @@
+// - 正则 字符串
+// 1. arr.join()
+function createPhoneNumber(numbers) {
+    return numbers.join('').replace(/(\d{3})(\d{3})(\d{4})/,'($1) $2-$3')
+}
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
