1f43e42c83c07b723a1e355ab5a5b9ea50810fe2
Quan-HHH <1411514716@qq.com>
2020-07-04 19:04:29 +0800

    手写reduce

--- js/reduce/1.js
+++ js/reduce/1.js
@@ -1,17 +1,17 @@
-function reduce(arr, reduceCallback, initailValue) {
+function reduce(arr, reduceCallback, initialValue) {
     if(!Array.isArray(arr) || !arr.length || typeof reduceCallback !== 'function') {
         return [];
     } else {
-        // 1. initailValue false  少执行一次
-        let hasInitialValue = initailValue !== undefined;
-        let value;
-        for() {
-            reduceCallback()
+        // 1. initialValue false  少执行一次
+        let hasInitialValue = initialValue !== undefined;
+        let value = hasInitialValue ? initialValue : arr[0];
+        for(let i = hasInitialValue ? 0 : 1; i < arr.length; i++) {
+            value = reduceCallback(value, arr[i], i, arr)
         }
         return value;
     }
 }
 
-reduce([1,2,3,4], function(pre, cur, index, ) {
-
-})
+console.log(reduce([1,2,3,4], function(pre, cur, index, arr) {
+    return pre + cur;
+}));
--- js/reduce/readme.md
+++ js/reduce/readme.md
@@ -2,3 +2,8 @@
 如果没有给initial_val 那数组的第一项 就是initial_val
 否则 initial_val 开始 + arr[0]
 arguments  类数组  ->  真正的数组  Array.from
+
+手写代码方法:
+    1. 原理
+    2. 从返回值入手 由结果向前推
+    2. 分布式写法
