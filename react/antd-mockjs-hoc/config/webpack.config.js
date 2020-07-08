c1e10dba12c95ba1d3c3c9f6c74eb83d5f080f40
Quan-HHH <1411514716@qq.com>
2020-07-09 00:41:06 +0800

    create-react-app 配置 stylus

--- react/antd-mockjs-hoc/config/webpack.config.js
+++ react/antd-mockjs-hoc/config/webpack.config.js
@@ -348,6 +348,14 @@ module.exports = function(webpackEnv) {
           // match the requirements. When no loader matches it will fall
           // back to the "file" loader at the end of the loader list.
           oneOf: [
+            {
+              test: /\.styl$/,
+              use: [
+                require.resolve('style-loader'),
+                require.resolve('css-loader'),
+                require.resolve('stylus-loader')
+              ]
+            },
             // "url" loader works like "file" loader except that it embeds assets
             // smaller than specified limit in bytes as data URLs to avoid requests.
             // A missing `test` is equivalent to a match.
