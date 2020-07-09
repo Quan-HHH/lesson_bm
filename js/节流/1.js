cbe002cccc953e25aae38fa5d4f0e975ee31ce41
Quan-HHH <1411514716@qq.com>
2020-07-09 20:03:42 +0800

    防抖与节流

---
+++ js/节流/1.js
@@ -0,0 +1,22 @@
+function throttled2(func, wait) {
+    var previous = 0;
+    var time = null;
+    return function() {
+        var now = new Date().getTime();
+        const remain = wait - (now - previous);
+        if(now - previous > wait) {
+            console.log(123)
+            func();
+            previous = now;
+        } else if(!time) {
+            time = setTimeout(() => {
+                func();
+                console.log(12212121)
+                time = null;
+                // 时间戳只运用于头事件
+                // 之后的事件 时间戳 和 timeout 是互竞的
+                previous = new Date().getTime();
+            }, remain);
+        }
+    }
+}
