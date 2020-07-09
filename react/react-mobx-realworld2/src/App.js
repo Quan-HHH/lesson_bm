8b846b1c6ec6f9ad5159f1ac867ea0caaccc12d2
Quan-HHH <1411514716@qq.com>
2020-07-09 20:04:40 +0800

    mobx

--- react/react-mobx-realworld2/src/App.js
+++ react/react-mobx-realworld2/src/App.js
@@ -3,7 +3,7 @@ import { Provider } from 'mobx-react';
 import articleStore from './store/articleStore'
 import Home from './pages/home'
 import logo from './logo.svg';
-import 'util/request.js'
+import './util/request.js'
 import './App.css';
 
 const store = {
