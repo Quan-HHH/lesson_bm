12bb00721a6d45e6b08fe2de7497865ebf47da84
Quan-HHH <1411514716@qq.com>
2020-07-10 20:23:05 +0800

    mobx

--- react/react-mobx-realworld2/src/pages/home/index.jsx
+++ react/react-mobx-realworld2/src/pages/home/index.jsx
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react'
+import { Pagination } from 'antd'
 
+// 数据都定义在store里面 组件只负责ui
 // inject 注入 这个组件要用哪个store的数据 就注入谁 注入完在props上面取就行
 // 注入完再observer一下 就行
 @inject('articleStore')
 @observer 
 class Home extends Component {
+    componentDidMount() {
+        this.props.articleStore.getArticle('all')
+    }
+    handlePaginationChange = (page) => {
+        this.props.articleStore.getArticle('all', page - 1)
+    }
     state = {  }
     render() { 
+        const { total, LIMIT } = this.props.articleStore
         return ( 
             <div>
                 {/* 数据放到这个组件的props上面了 */}
                 {this.props.articleStore.articles.all.length}
+                <Pagination total={total} pageSize={LIMIT} defaultCurrent={1}
+                onChange={this.handlePaginationChange}
+                />
             </div>
          );
     }
--- react/react-mobx-realworld2/src/store/articleStore.js
+++ react/react-mobx-realworld2/src/store/articleStore.js
@@ -4,17 +4,20 @@ const LIMIT = 10;
 class ArticleStore {
     // 用observerable修饰过的变量 只要变量被修改 页面就会重新渲染
     // @ es中的新语法 装饰器
+    LIMIT = LIMIT
     @observable articles = {
-        all: [],
-        cars: []
+        all: []
     }
 
+    @observable total = 0;
+
+    // 繁杂的逻辑 尽量写到store来
     // 行为
     @action
-    getArticle(tag, offset) {
+    getArticle(tag, offset = 0) {
         axios.get('/articles',{
             params: {
-                tag,
+                tag: tag === 'all' ? null : tag,
                 offset,
                 limit: LIMIT
             }
@@ -23,6 +26,7 @@ class ArticleStore {
             // 修改store
             // 不用setState mapDispatch
             this.articles[tag] = res.articles
+            this.total = res.articlesCount
         })
     }
 
