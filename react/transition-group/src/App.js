ed315cc2ac05848c65c1e21517fd3a9577a72932
Quan-HHH <1411514716@qq.com>
2020-07-29 21:49:45 +0800

    react-transition-group

---
+++ react/transition-group/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CssTransitionGroupDemo from './CssTransition';
+import Tabs from './Tabs'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CssTransitionGroupDemo />
+      <Tabs />
+    </div>
+  );
+}
+
+export default App;
