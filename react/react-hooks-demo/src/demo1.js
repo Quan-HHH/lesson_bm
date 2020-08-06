893eef424cd63e76236fcff2e2bb65499ad7d9b5
Quan-HHH <1411514716@qq.com>
2020-08-06 23:44:07 +0800

    useReducer useRef

--- react/react-hooks-demo/src/demo1.js
+++ react/react-hooks-demo/src/demo1.js
@@ -31,55 +31,178 @@
 //     )
 // }, isSameRange)
 
-import React, {memo} from 'react'
-export const MyComponent = () => {
-    const [filter, setFilter] = React.useState("")
-    const [userCollection, setUserCollection] = React.useState([]);
-    const [userInfo, setUserInfo] = React.useState({
-        name: 'John',
-        lastname: 'Doe'
-    });
+// import React, {memo} from 'react'
+// export const MyComponent = () => {
+//     const [filter, setFilter] = React.useState("")
+//     const [userCollection, setUserCollection] = React.useState([]);
+//     const [userInfo, setUserInfo] = React.useState({
+//         name: 'John',
+//         lastname: 'Doe'
+//     });
 
-    setTimeout(() => {
-        setUserInfo({
-            ...userInfo,
-            name: 'John'
-        })
-    }, 2000)
-    return (
-        <>
-            <DisplayUsername name={userInfo.name} />
-            <input
-                type="text"
-                value={userInfo.name}
-                onChange={event => setUserInfo({
-                    ...userInfo,
-                    name: event.target.value
-                })} />
-        </>
-    )
+//     setTimeout(() => {
+//         setUserInfo({
+//             ...userInfo,
+//             name: 'John'
+//         })
+//     }, 2000)
+//     return (
+//         <>
+//             <DisplayUsername name={userInfo.name} />
+//             <input
+//                 type="text"
+//                 value={userInfo.name}
+//                 onChange={event => setUserInfo({
+//                     ...userInfo,
+//                     name: event.target.value
+//                 })} />
+//         </>
+//     )
 
-    // React.useEffect(() => {
-    //     fetch(`https://jsonplaceholder.typicode.com/users?name_like=${filter}`)
-    //     .then(response => response.json())
-    //     .then(json => setUserCollection(json));
-    // }, [filter])
-    // return (
-    //     <div>
-    //         <input 
-    //         value={filter}
-    //         onChange={(e) => {setFilter(e.target.value)}}
-    //         />
-    //         <ul>
-    //             {userCollection.map((user, index) => 
-    //                 <li key={index}>{user.name}</li>
-    //             )}
-    //         </ul>
-    //     </div>
-    // )
+// React.useEffect(() => {
+//     fetch(`https://jsonplaceholder.typicode.com/users?name_like=${filter}`)
+//     .then(response => response.json())
+//     .then(json => setUserCollection(json));
+// }, [filter])
+// return (
+//     <div>
+//         <input 
+//         value={filter}
+//         onChange={(e) => {setFilter(e.target.value)}}
+//         />
+//         <ul>
+//             {userCollection.map((user, index) => 
+//                 <li key={index}>{user.name}</li>
+//             )}
+//         </ul>
+//     </div>
+// )
+// }
+
+// export const DisplayUsername = memo(props => {
+//     console.log('只有在name发生改变时才更新')
+//     return <h3>{props.name}</h3>
+// })
+
+// import React, { useState, useEffect, useRef } from 'react'
+
+// export const MyComponent = () => {
+//     const [message, setMessage] = useState("initial message");
+//     const [second, setSecond] = useState(0);
+//     const secondRef = useRef()
+
+//     useEffect(() => {
+//         setTimeout(() => {
+//             console.log(second);
+//             setSecond(1);
+//             secondRef.current = 1;
+//         }, 1000)
+//         setTimeout(() => {
+//             setMessage(`Total second: ${secondRef.current}`)
+//         }, 2000)
+//     }, [])
+//     return (
+//         <>
+//             <h3>{message}</h3>
+//             <h4>{second}</h4>
+//         </>
+//     )
+// }
+
+// import React, { useState, useRef } from 'react';
+
+// export const MyComponent = () => {
+//     const [visible, setVisible] = useState(false);
+//     return (
+//         <>
+//             {visible && <MyChildComponent />}
+//             <button
+//                 onClick={() => { setVisible(!visible) }}>
+//                 Toggle Child component visible</button>
+//         </>
+//     )
+// }
+
+// export const MyChildComponent = () => {
+//     const [filter, setFilter] = useState("");
+//     const [userCollection, setUserCollection] = React.useState([]);
+//     const mountedRef = useRef(false);
+
+//     React.useEffect(() => {
+//         mountedRef.current = true;
+//         return () => (mountedRef.current = false)
+//     })
+
+//     React.useEffect(() => {
+//         fetch(`https://jsonplaceholder.typicode.com/users?name_like=${filter}`)
+//             .then(response => response.json())
+//             .then(json => setUserCollection(json));
+//     }, [filter])
+//     return (
+//         <div>
+//             <input
+//                 value={filter}
+//                 onChange={(e) => { setFilter(e.target.value) }}
+//             />
+//             <ul>
+//                 {userCollection.map((user, index) =>
+//                     <li key={index}>{user.name}</li>
+//                 )}
+//             </ul>
+//         </div>
+//     )
+// }
+
+// 数据
+import React, {useReducer} from 'react'
+const userInfoReducer = (state, action) => {
+    switch (action.type) {
+        case "setusername":
+            return {
+                ...state,
+                name: action.payload,
+            }
+        case "setlastname":
+            return {
+                ...state,
+                lastname: action.payload
+            }
+        default:
+            return state;
+    }
 }
 
-export const DisplayUsername = memo(props => {
-    console.log('只有在name发生改变时才更新')
-    return <h3>{props.name}</h3>
+export const MyComponent = () => {
+    const [reducer, dispatch] = useReducer(userInfoReducer, {
+        name: "John",
+        lastname: "Doe"
+    })
+
+    return (
+        <>
+            <h3>{reducer.name}{reducer.lastname}</h3>
+            <EditUsername name={reducer.name} dispatch={dispatch} />
+            <input type="text" value={reducer.lastname}
+            onChange={e => dispatch({
+                type: "setlastname",
+                payload: e.target.value
+            })}
+            />
+        </>
+    )
+}
+
+const EditUsername = React.memo(props => {
+    console.log("name改变时我才会重新渲染嗷")
+    return (
+        <input type="text"
+        value={props.name}
+        onChange={
+            e => props.dispatch({
+                type: "setusername",
+                payload: e.target.value
+            })
+        }
+        />
+    )
 })
