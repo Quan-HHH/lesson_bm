3bc0747316b35f7da054284786bbf52f7925fa6b
Quan-HHH <1411514716@qq.com>
2020-08-15 21:46:51 +0800

    ins全栈项目

---
+++ fullstack/instagram_backend/app/router.ts
@@ -0,0 +1,16 @@
+import { Application } from 'egg';
+
+export default (app: Application) => {
+  const { controller, router } = app;
+
+  // controlller url => controller -> render view
+  // 路由列表 nginx 服务器代理 ip http://www.baidu.com -> ip -> nginx 80 -> 
+  // 后端路由 / MVC Contriller /index.html #root
+  // 前端路由 react build js 挂载 SPA 更快 useEffect -> reducer action -> 
+  // api -> 后台/api/user  /api 后端 吐数据的地方
+  router.get('/', controller.home.index);
+  // 登录接口 /api/login
+  router.get('/api/login', controller.login.index);
+};
+
+ 
---
+++ fullstack/readme.md
@@ -0,0 +1,12 @@
+阿里系
+    React(Antd 前端UI) + ts(大型项目的保证) + egg(koa 企业) mysql/mongodb
+
+1. fullstack架构
+    fromend React Antd/Element 企业级 PC 10%/mobile 网页访问 +　微信公众号 工作量大 移动开发 70%/PC ADMIN　后台开发　组件化功能/小程序　App 20%
+    前端 UI界面 + 后台管理 + App类化(小程序 unionapp flutter RN)
+
+    React 思想 UI + MVVM + 数据管理
+
+2. egg
+   - MVC Model View Contorller
+   - koa的封装 轻量级
