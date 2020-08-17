de035791d4813b68c56d709be80f06de1c2c4b3b
Quan-HHH <1411514716@qq.com>
2020-08-17 22:54:40 +0800

    从零构建一个react项目

--- babel/src/App.jsx
+++ babel/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import styles from './style.css';
 
 export default function App() {
     return (
-        <div>
+        <div 
+        // className="swiper-container"
+        className={styles["swiper-container"]}
+        >
+            {/* ./ 以当前路径为基准 http://baidu.com/path/a/b/avatr.jpg */}
+            {/* /  http://baidu.com/avatr.jpg */}
+            <img src="./avatar.JPG" alt=""/>
             this is app
         </div>
     )
--- babel/webpack.config.js
+++ babel/webpack.config.js
@@ -1,10 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CopyPlugin = require('copy-webpack-plugin');
+
 const config = {
     entry: './src/index.js',
     resolve: {
         extensions: ['.js', '.jsx']
     },
     devServer: {
-        contentBase: './dist',
+        contentBase: './public',
         hot: true
     },
     module: {
@@ -13,9 +17,39 @@ const config = {
                 test: /(.js|.jsx)$/,
                 use: ['babel-loader'],
                 exclude: /node_modules/
-            }
+            }, {
+                test: /\.css$/i,
+                use: ['style-loader', 
+                {
+                    loader: 'css-loader',
+                    options: {
+                        modules: {
+                            mode: 'local',
+                            localIdentName: '[hash:base64:5]'
+                        }
+                    }
+                }],
+                // css-loader css这个模块
+                // style-loader css内容插到html的style标签内
+            },
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html', // 模板来源文件
+            filename: 'index.html'  // 生成的模板文件的名字
+        }),
+        new BundleAnalyzerPlugin(),
+        // 静态资源做一个拷贝 拷贝到dist目录下
+        new CopyPlugin({
+            patterns: [
+                { from: './public/avatar.JPG', to: './' },
+            ],
+        }),
+    ]
 }
 
 module.exports = config
+
+// dist 存着的就是项目最终打包完的结果
+// dist 就是最终上线的文件夹
