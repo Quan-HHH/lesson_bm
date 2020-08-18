40653eb6dd58807e5db34b1b0e4a17a0f8012552
Quan-HHH <1411514716@qq.com>
2020-08-18 22:53:12 +0800

    object

--- books/js_you_dont_know/chapter3-对象/1.js
+++ books/js_you_dont_know/chapter3-对象/1.js
@@ -2,3 +2,9 @@ var str = 'asd';  //string 不是String的实例
 var str1 = new String('123');  //object 是String的实例
 
 console.log(typeof str, typeof str1, str instanceof String, str1 instanceof String)
+
+var myObj = {
+    a: 2
+}
+
+console.log(Object.getOwnPropertyDescriptor(myObj, 'a'))
