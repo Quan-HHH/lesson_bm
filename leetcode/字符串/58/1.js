7c813875fe81df335103b298c91724e9cdaaa302
Quan-HHH <1411514716@qq.com>
2020-08-24 23:18:36 +0800

    最后一个单词的长度

--- leetcode/字符串/58/1.js
+++ leetcode/字符串/58/1.js
@@ -1,15 +1,6 @@
 var lengthOfLastWord = function(s) {
-    if(s == '') return 0;
-    else if(!s.trim().includes(' ')) return s.trim().length;
-    s = s.split('').reverse().join('');
-    console.log(s)
-    let length = 0;
-    for(let i of s) {
-        if(i === ' ') {
-            break;
-        }
-        length++;
-    }
-    return length;
+    if(s === '') return 0;
+    return s.trim().split(' ').reverse()[0].length;
 };
-lengthOfLastWord('Hello World');
+
+console.log(lengthOfLastWord('Hello World')) ;
