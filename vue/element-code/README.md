e5daa1dbcaa4435d9901e8200e819d19a5069095
Quan-HHH <1411514716@qq.com>
2020-08-25 15:17:53 +0800

    element-ui源码分析

---
+++ vue/element-code/README.md
@@ -0,0 +1,16 @@
+- vue-cli 4, vue 3.0变化太大
+
+- vue create project-name
+    vue 2.0  vue init webpack project-name 传统 现在流行
+    vue 3.0  vue create project-name
+    - PWA 渐进式网页应用
+    - Router Vuex 全家桶
+    - CSS　Pre-processors stylus vw autoprefixer
+        编译 sass/stylus 插件 autoprefixer 用于做 兼容性的前缀
+        
+    - linter 代码风格确保文件 ;  ""/''  
+        eslint 保证大家代码风格一致
+        不用的变量不要定义 要删除干净
+        对齐代码
+        在公司中 一般js结尾不加;
+    - unit Testing  E2E  测试
---
+++ vue/element-code/src/components/ElButton.vue
@@ -0,0 +1,52 @@
+<template>
+<button
+    @click="handleClick"
+    class="el-button"
+    :class="[
+        type?'el-button--'+type:'',
+        {
+            'is-round': round,
+            'is-circle': circle
+        }
+    ]"
+    >
+    <!-- {{type}} - {{tit}} -->
+    <!-- Button -->
+    <slot></slot>
+</button>
+</template>
+<script>
+// props 检查 解构
+export default {
+    data() {
+        // 组件的内部状态
+        return {
+            tit: 'hello'
+        }
+    },
+    props: {
+        type: { 
+            type: String,
+            default: 'default'
+        },
+        round: {
+            type: Boolean
+        },
+        circle: {
+            type: Boolean
+        }
+    },
+    created() {
+        console.log(this.type, this.tit)
+    },
+    methods: {
+        handleClick(evt) {
+            // $emit vue中做事件处理的一种方案
+            this.$emit('click', evt);
+        }
+    }
+}
+</script>
+<style lang="stylus">
+
+</style>
